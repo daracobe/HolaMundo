b89804353bfe5ad4d7ad002d3018489eba9a724f
Dariana Contreras <daracobe@gmail.com>
2012-11-04 21:58:14 -0430

    Creacion de aplicacion
    Creacion de la aplicacion y la clase artista

---
+++ PracticaJAVA/src/practicajava/artista.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicajava;
+
+/**
+ *
+ * @author Contreras Berbesi
+ */
+public class artista {
+    
+    private String nombre;
+    private int anioNacimiento;
+    
+    
+    public artista(String nomb, int anio){
+        nombre = nomb;
+        anioNacimiento = anio;
+    }
+    
+}
