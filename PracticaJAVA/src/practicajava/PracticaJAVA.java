1cdaf971ea0e8d2cd313fccbecf00ded215789db
Dariana Contreras <daracobe@gmail.com>
2012-11-04 23:26:03 -0430

    Datos de obra
    Ingresar los datos de la clase obra.

--- PracticaJAVA/src/practicajava/PracticaJAVA.java
+++ PracticaJAVA/src/practicajava/PracticaJAVA.java
@@ -22,8 +22,11 @@ public class PracticaJAVA {
         
         int opcion = 0;
         int cantidadObra = 0;
+        int cantidadArtista = 0;
         String nombArtista = "";
-        obra objObra;
+        String titulo = "";
+        int anioEdicion = 0;
+        List<obra> listaObras = null ;
         artista objArtista;
         
         System.out.println("2.- Ingresar Libro");
@@ -35,12 +38,7 @@ public class PracticaJAVA {
 
         opcion = Integer.parseInt(leer.readLine());
         
-         System.out.println("Ingrese cantidad de obras: ");
-         cantidadObra = Integer.parseInt(leer.readLine());
-         
-                 
-         System.out.println("Ingrese nombre del artista: ");
-         nombArtista = Integer.parseInt(leer.readLine());
+      
          
         switch(opcion)
         {
@@ -48,20 +46,35 @@ public class PracticaJAVA {
                 break;
                 
             case 2:
-                for(int )
+            case 3:
+            case 4:
+                System.out.println("Ingrese cantidad de obras: ");
+                cantidadObra = Integer.parseInt(leer.readLine());
+                        
+                System.out.println("Ingrese nombre del artista de la obra: ");
+                nombArtista = leer.readLine();
+                
+                //Busco el artista a asignar la obra
+                for(int cArtis = 0; cArtis < cantidadArtista; cArtis++)
                 {
-                    
+                    //objArtista = ;
                 }
+                //Creo el objeto obra para esa artista
                 for(int cObra = 0 ; cObra < cantidadObra; cObra++)
                 {
+                    obra auxObra = new obra();
+                   
+                    System.out.println("Ingrese titulo de la obra");
+                    titulo = leer.readLine();
+                    System.out.println("Ingrese año de edicion");
+                    anioEdicion = Integer.parseInt(leer.readLine());
                     
-                    
-                    
+                    auxObra.setTitulo(titulo);
+                    auxObra.setAnioEdicion(anioEdicion);
+                    listaObras.add(auxObra);
                 }
                 
-                
-                
-                break;
+            
             
             
         }
