76e7baf30622c30309548b1c0b6f039874251983
Oswaldo Cáceres <oswal026@gmail.com>
2012-11-07 00:08:28 -0430

    Creacion de los metodos principales de la clase Pelicula

--- PracticaJAVA/src/practicajava/Pelicula.java
+++ PracticaJAVA/src/practicajava/Pelicula.java
@@ -3,11 +3,44 @@
  * and open the template in the editor.
  */
 package practicajava;
-
+import java.io.*;
 /**
  *
  * @author Oswaldo
  */
-public class Pelicula {
+public class Pelicula extends Obra {
+    private String productora;
+    private Artista[] interpretes;
+   
+    public Pelicula(String titulo, Artista autor, int anioEdicion, String productora, Artista[] interpretes) {
+        this.setTitulo(titulo);
+        this.setAutor(autor);
+        this.setAnioEdicion(anioEdicion);
+        this.productora = productora;
+        this.interpretes = interpretes;
+    }
     
+    public Artista[] getInterpretes(){
+        return this.interpretes;
+    }
+    
+    public void setInterpretes(Artista[] interpretes){
+        this.interpretes = interpretes;
+    }
+    
+    public String getProductora(){
+        return this.productora;
+    }
+    
+    public void setProductora(String productora){
+        this.productora = productora;
+    }
+    
+    public void imprimir() {
+        System.out.println("Título: " + this.getTitulo());
+        System.out.println("Autor: " + this.getAutor());
+        System.out.println("Año de Edición: " + this.getAnioEdicion());
+        System.out.println("Interpretes: " + this.getInterpretes());
+        System.out.println("Productora: " + this.getProductora());
+    }
 }
